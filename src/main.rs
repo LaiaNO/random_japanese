e6c4532fe0136bd99d258ef48d0544f44cbdb626
LaiaNO <41057949+LaiaNO@users.noreply.github.com>
2024-10-11 17:44:00 -0400

    Edit main code to select category
    Create a new main menu that allows you to select a category. The program will loop through each item in the selected category, and you can review them one by one. Type 'exit' at any point to close the program.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,54 @@ use std::fs;
 use std::io;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let files = fs::read_dir("Words")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    println!("Select one category:");
+    for file in files.iter().enumerate() {
+        println!(
+            "{}: {}",
+            file.0,
+            file.1
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .trim_end_matches(".txt")
+        );
+    }
+    let mut category = String::new();
+    io::stdin().read_line(&mut category).unwrap();
+
+    // Importar archivo
+    let category: usize = category.trim().parse().unwrap();
+    let message = fs::read_to_string(files[category].clone())?;
+    let mut user_entry = String::new();
+    // Get variables
+    let mut rng = rand::thread_rng();
+    loop {
+        user_entry.clear();
+        let var = message
+            .lines()
+            .map(|line| line.split_once(',').expect("No hay ,"))
+            .choose(&mut rng)
+            .expect("Siempre hay lineas para elegir");
+
+        // Devolver llave random
+        println!("{}", var.0);
+        // Enter para ver respuesta
+        io::stdin().read_line(&mut user_entry).unwrap();
+        if user_entry.trim() == "exit" {
+            break;
+        }
+        println!("{}", var.1);
+    }
+    Ok(())
+}
+
+#[allow(unused)]
+fn main_second_try() -> Result<(), Box<dyn Error>> {
     // Importar archivo
     let message = fs::read_to_string("words.txt")?;
     // Get variables
