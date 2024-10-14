b03724390cf87eaa340676e9e240c4c9bbda9ded
LaiaNO <41057949+LaiaNO@users.noreply.github.com>
2024-10-14 10:19:23 -0400

    Refactor for More Idiomatic Rust Implementations
    Created Category and Words modules. Refactoring is still in progress for optimization and cleanup.

--- src/main.rs
+++ src/main.rs
@@ -4,49 +4,42 @@ use std::error::Error;
 use std::fs;
 use std::io;
 
+mod category;
+mod words;
+
+use category::Category;
+use words::Words;
+
 fn main() -> Result<(), Box<dyn Error>> {
     let files = fs::read_dir("Words")?
-        .map(|res| res.map(|e| e.path()))
+        .map(|res| res.map(|e| Category(e.path())))
         .collect::<Result<Vec<_>, io::Error>>()?;
 
     println!("Select one category:");
-    for file in files.iter().enumerate() {
-        println!(
-            "{}: {}",
-            file.0,
-            file.1
-                .file_name()
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .trim_end_matches(".txt")
-        );
+    for (index, category) in files.iter().enumerate() {
+        println!("{}: {}", index, category.file_name());
     }
     let mut category = String::new();
-    io::stdin().read_line(&mut category).unwrap();
+    io::stdin().read_line(&mut category)?;
 
     // Importar archivo
-    let category: usize = category.trim().parse().unwrap();
-    let message = fs::read_to_string(files[category].clone())?;
+    let category: usize = category.trim().parse()?;
+    let message = fs::read_to_string(&files[category].0)?;
+    let mut words = Words::new(&message);
+
     let mut user_entry = String::new();
-    // Get variables
-    let mut rng = rand::thread_rng();
     loop {
         user_entry.clear();
-        let var = message
-            .lines()
-            .map(|line| line.split_once(',').expect("No hay ,"))
-            .choose(&mut rng)
-            .expect("Siempre hay lineas para elegir");
+        let (eng, jap) = words.random().expect("Siempre hay lineas para elegir");
 
         // Devolver llave random
-        println!("{}", var.0);
+        println!("{}", eng);
         // Enter para ver respuesta
         io::stdin().read_line(&mut user_entry).unwrap();
         if user_entry.trim() == "exit" {
             break;
         }
-        println!("{}", var.1);
+        println!("{}", jap);
     }
     Ok(())
 }
